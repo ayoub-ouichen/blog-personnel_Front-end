7dd3793b98618828836e2645d857065205735848
Ouichen Ayoub <ayoub.wichen705@gmail.com>
2023-08-16 11:44:13 +0100

    quill resize image

--- src/app/blogs/contenu/poste/poste.component.ts
+++ src/app/blogs/contenu/poste/poste.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import * as QuillNamespace from 'quill';
+import ImageResize from 'quill-image-resize-module';
+let Quill: any = QuillNamespace;
+Quill.register('modules/imageResize', ImageResize);
+
 
 @Component({
   selector: 'app-poste',
@@ -9,10 +14,27 @@ import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
   encapsulation: ViewEncapsulation.None
 })
 export class PosteComponent implements OnInit {
+  editor_modules = {};
+
   blog: FormGroup;
   editorText: any;
 
   constructor() {
+    this.editor_modules = {
+      toolbar: {
+        container: [
+          [{ 'font': [] }],
+          [{ 'size': ['small', false, 'large', 'huge'] }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ 'header': 1 }, { 'header': 2 }],
+          [{ 'color': [] }, { 'background': [] }],
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+          [{ 'align': [] }],
+          ['link', 'image']
+        ]
+      },
+      imageResize: true
+    };
   }
 
   ngOnInit(): void {
